735cc03e0b8e2f95e28aea4f461888456fd07e93
James Zhang <james.zhang@ippen-digital.de>
2022-08-23 15:49:34 +0200

    rename MultiBootstrapDonatorExtractor

---
+++ src/test/java/de/id/quarkus/kafka/testing/reactivemessaging_multibootstrap/MultiBootstrapDonatorExtractorTest.java
@@ -0,0 +1,60 @@
+package de.id.quarkus.kafka.testing.reactivemessaging_multibootstrap;
+
+import de.id.avro.Donation;
+import de.id.avro.Donator;
+import de.id.quarkus.kafka.testing.ConfluentStack;
+import de.id.quarkus.kafka.testing.ConfluentStackClient;
+import de.id.quarkus.kafka.testing.scenarios.DonatorExtractorProfile;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.ResourceArg;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+@QuarkusTestResource(value = ConfluentStack.class, initArgs = {
+        @ResourceArg(value="incoming", name="source-topic"),
+        @ResourceArg(value="outgoing", name="target-topic")
+})
+@TestProfile(DonatorExtractorProfile.class)
+class MultiBootstrapDonatorExtractorTest {
+
+    public static final int MAX_CONSUMER_WAIT_TIME = 5000;
+    private static final String SOURCE_TOPIC = "reactivemessaging.source-topic";
+    private static final String TARGET_TOPIC = "reactivemessaging.target-topic";
+
+    ConfluentStackClient testClusterClient;
+
+    @BeforeEach
+    void setUp() {
+        testClusterClient.createTopics(SOURCE_TOPIC, TARGET_TOPIC);
+    }
+
+    @Test
+    void shouldExtractADonatorOutOfEveryDonation() throws InterruptedException, ExecutionException, TimeoutException {
+        Donation donation = new Donation("Max", "Mustermann", 10.0, 111);
+        List<Donation> donationToSend = IntStream.range(0, 10).mapToObj(i -> donation).collect(Collectors.toList());
+
+        Future<List<Donator>> receiveFuture = testClusterClient.waitForRecords(TARGET_TOPIC, "testConsumerGroup", donationToSend.size(), StringDeserializer.class);
+
+        testClusterClient.sendRecords(SOURCE_TOPIC, donationToSend, StringSerializer.class, (index, event) -> String.valueOf(index));
+
+        List<Donator> receivedDonators = receiveFuture.get(MAX_CONSUMER_WAIT_TIME, TimeUnit.MILLISECONDS);
+
+        assertThat(receivedDonators).hasSameSizeAs(donationToSend);
+    }
+}
